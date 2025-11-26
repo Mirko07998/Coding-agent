fc0ddb3ca22d98d09ca9e40e6042da1d083f1d95
lun1bg <lun1bg@bosch.com>
2025-11-26 15:00:05 +0100

    added response type

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes import jira
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
 
